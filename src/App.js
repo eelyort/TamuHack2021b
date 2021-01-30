4d4c70dea5743641100b368afbec441d85dbcc3e
Troy Lee <troylee3006@gmail.com>
2021-01-30 17:49:04 -0600

    custom and app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import Header from "./Header/Header.js";
+import Timer from "./Timer/Timer.js";
+import Map from "./Map/Map.js";
+import Options from "./Options/Options.js";
+import Leaderboards from "./Leaderboards/Leaderboards.js";
+
 import './App.css';
-import Map from "./Map/Map.js"
+
 function App() {
-  return (
-    <div className="App">
-      <Map/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <div className={'timer-div'}>
+                <Timer />
+            </div>
+            <div className={'map-div'}>
+                <Map />
+            </div>
+            <div className={'options-div'}>
+                <Options />
+            </div>
+            <div className={'leaderboards-div'}>
+                <Leaderboards />
+            </div>
+        </div>
+    );
 }
 
 export default App;
