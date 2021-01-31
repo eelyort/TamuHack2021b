91924919613bc1c29602cb0522391e35788fd2df
Abhishek22002 <41200654+Abhishek22002@users.noreply.github.com>
2021-01-30 18:32:14 -0600

    map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Map from "./Map/Map.js"
+import { withScriptjs } from "react-google-maps";
+
 function App() {
+  const MapLoader = withScriptjs(Map);
   return (
     <div className="App">
-      <Map/>
+      <MapLoader
+      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAOn4IKOryQBul-tiLGCvsXEzAJGNKtX9Q"
+      loadingElement={<div style={{ height: `100%` }} />}
+      />
     </div>
   );
 }
--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,29 +1,76 @@
 import React , { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import {  GoogleMap, DirectionsRenderer, withGoogleMap, withScriptjs, google} from 'react-google-maps';
 
 const mapStyles = {
   width: '100%',
   height: '100%'
 };
-
-export class MapContainer extends React.Component {
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={
+class Map extends Component {
+    state = {
+        directions: null
+      };
+    
+      componentDidMount() {
+        const directionsService = new google.maps.DirectionsService();
+    
+        const origin = { lat: 40.756795, lng: -73.954298 };
+        const destination = { lat: 41.756795, lng: -78.954298 };
+    
+        directionsService.route(
           {
-            lat: -1.2884,
-            lng: 36.8233
+            origin: origin,
+            destination: destination,
+            travelMode: google.maps.TravelMode.DRIVING
+          },
+          (result, status) => {
+            if (status === google.maps.DirectionsStatus.OK) {
+              this.setState({
+                directions: result
+              });
+            } else {
+              console.error(`error fetching directions ${result}`);
+            }
           }
-        }
-      />
-    );
-  }
+        );
+      }
+    
+      render() {
+        const GoogleMapExample = withGoogleMap(props => (
+          <GoogleMap
+            defaultCenter={{ lat: 40.756795, lng: -73.954298 }}
+            defaultZoom={13}
+          >
+            <DirectionsRenderer
+              directions={this.state.directions}
+            />
+          </GoogleMap>
+        ));
+    
+        return (
+          <div>
+            <GoogleMapExample
+              containerElement={<div style={{ height: `500px`, width: "500px" }} />}
+              mapElement={<div style={{ height: `100%` }} />}
+            />
+          </div>
+        );
+      }
 }
-
+/*
+<Map
+    google={this.props.google}
+    zoom={14}
+    style={mapStyles}
+    initialCenter={
+    {
+        lat: -1.2884,
+        lng: 36.8233
+    }
+    }
+/>
 export default GoogleApiWrapper({
     apiKey: 'AIzaSyAOn4IKOryQBul-tiLGCvsXEzAJGNKtX9Q'
-  })(MapContainer);
+  })(Map);
+  */
+
+export default Map
