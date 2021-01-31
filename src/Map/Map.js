99d6668405e5a800dbf2325bc5df827a7efeee73
Troy Lee <troylee3006@gmail.com>
2021-01-30 20:32:40 -0600

    made map routing easily accessible from outside

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -6,59 +6,109 @@ const mapStyles = {
   height: '100%'
 };
 class Map extends Component {
-    state = {
-        directions: null
-      };
-    
-      componentDidMount() {
-        const directionsService = new window.google.maps.DirectionsService();
-    
-        const origin = { lat: 40.756795, lng: -73.954298 };
-        const destination = { lat: 40.757895, lng: -73.954298 };
-    
-        directionsService.route(
-          {
-            origin: origin,
-            destination: destination,
-            travelMode: window.google.maps.TravelMode.WALKING
-          },
-          (result, status) => {
-            console.log("result:")
-            console.log(result)
-            console.log("status:")
-            console.log(result.routes[0].legs[0].duration.value)
-            if (status === window.google.maps.DirectionsStatus.OK) {
-              this.setState({
-                directions: result
-              });
-            } else {
-              console.error(`error fetching directions ${result}`);
-            }
-          }
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      directions: null
+    };
+
+    // for use in routeMe
+    this.directionsService = new window.google.maps.DirectionsService();
+
+    // bind functions
+    this.routeMe = this.routeMe.bind(this);
+    this.routeMeCallback = this.routeMeCallback.bind(this);
+  }
+  // specs:
+  //  startLocation: map { lat | latitude: NUMBER, lng | long | longitude: NUMBER }
+  //  endLocation: map { lat | latitude: NUMBER, lng | long | longitude: NUMBER }
+  //  callback: (time) => ???
+  //    if you (some other class) needs the time, make this "(time) => setState(...)" or something
+  //  examples:
+  //    routeMe({latitude: 0.1, long: 0.1}, {lat: 2, lng: -2})
+  //      makes the map display the path from [0.1, 0.1] to [2, -2]
+  routeMe(startLocation, endLocation, callback = (time) => null) {
+    const origin = {
+      lat: startLocation.lat ?? startLocation.latitude,
+      lng: startLocation.lng ?? startLocation.long ?? startLocation.longitude,
+    };
+    const destination = {
+      lat: endLocation.lat ?? endLocation.latitude,
+      lng: endLocation.lng ?? endLocation.long ?? endLocation.longitude,
+    };
+    const requestJSON = {
+      origin,
+      destination,
+      travelMode: window.google.maps.TravelMode.WALKING,
+    };
+
+    this.directionsService.route({...requestJSON}, this.routeMeCallback(callback));
+  }
+  // helper function for above
+  routeMeCallback(agentCallback) {
+    return ((result, status) => {
+      if (status === window.google.maps.DirectionsStatus.OK) {
+        this.setState({
+          directions: result
+        });
+        agentCallback(result.routes[0].legs[0].duration.value);
+      } else {
+        console.error(`error fetching directions ${result}`);
       }
+    });
+  }
     
-      render() {
-        const GoogleMapExample = withGoogleMap(props => (
-          <GoogleMap
-            defaultCenter={{ lat: 40.756795, lng: -73.954298 }}
-            defaultZoom={13}
-          >
-            <DirectionsRenderer
-              directions={this.state.directions}
-            />
-          </GoogleMap>
-        ));
-    
-        return (
-          <div>
-            <GoogleMapExample
-              containerElement={<div style={{ height: `500px`, width: "500px" }} />}
-              mapElement={<div style={{ height: `100%` }} />}
-            />
-          </div>
-        );
-      }
+  componentDidMount() {
+    // const directionsService = new window.google.maps.DirectionsService();
+
+    const origin = { lat: 40.756795, lng: -73.954298 };
+    const destination = { lat: 40.857895, lng: -73.954298 };
+
+    this.routeMe(origin, destination, (val) => console.log(`Default routeMe returning ${val} seconds`));
+
+    // directionsService.route(
+    //   {
+    //     origin: origin,
+    //     destination: destination,
+    //     travelMode: window.google.maps.TravelMode.WALKING
+    //   },
+    //   (result, status) => {
+    //     console.log("result:")
+    //     console.log(result)
+    //     console.log("status:")
+    //     console.log(result.routes[0].legs[0].duration.value)
+    //     if (status === window.google.maps.DirectionsStatus.OK) {
+    //       this.setState({
+    //         directions: result
+    //       });
+    //     } else {
+    //       console.error(`error fetching directions ${result}`);
+    //     }
+    //   }
+    // );
+  }
+
+  render() {
+    const GoogleMapExample = withGoogleMap(props => (
+      <GoogleMap
+        defaultCenter={{ lat: 40.756795, lng: -73.954298 }}
+        defaultZoom={13}
+      >
+        <DirectionsRenderer
+          directions={this.state.directions}
+        />
+      </GoogleMap>
+    ));
+
+    return (
+      <div>
+        <GoogleMapExample
+          containerElement={<div style={{ height: `500px`, width: "500px" }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
+    );
+  }
 }
 /*
 <Map
